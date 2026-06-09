dd1160d81622593ceb1435731a182678f3956957
Kagisosa <53257248+KagisoEasy@users.noreply.github.com>
2026-06-09 09:06:31 +0200

    Add ChromeOptions import to BaseClass

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -6,6 +6,8 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.ConfigReader;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 import java.io.IOException;
 
